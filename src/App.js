ad037e35b520144e1087d3db5d0269fba88fae9e
Kiko <sthyeon11#gmail.com>
2023-05-13 16:33:25 +0000

    make_dialog

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import styled, { css, ThemeProvider } from "styled-components";
 import Button from "./components/Button";
+import Dialog from "./components/dialog";
 
 const AppBlock = styled.div`
   width: 512px;
@@ -18,6 +19,22 @@ const ButtonGroup = styled.div`
 `;
 
 function App() {
+  const [dialog, setDialog] = useState(false);
+
+  const onClickBtn = () => {
+    setDialog(true);
+  };
+
+  const onConfirm = () => {
+    console.log("confirm");
+    setDialog(false);
+  };
+
+  const onCancel = () => {
+    console.log("cancel");
+    setDialog(false);
+  };
+
   return (
     <ThemeProvider
       theme={{
@@ -43,57 +60,69 @@ function App() {
         },
       }}
     >
-      <AppBlock>
-        <ButtonGroup>
-          <Button size="large">BUTTON</Button>
-          <Button>BUTTON</Button>
-          <Button size="small">BUTTON</Button>
-        </ButtonGroup>
+      <>
+        <AppBlock>
+          <ButtonGroup>
+            <Button size="large">BUTTON</Button>
+            <Button>BUTTON</Button>
+            <Button size="small">BUTTON</Button>
+          </ButtonGroup>
 
-        <ButtonGroup>
-          <Button color="gray" size="large">
-            BUTTON
-          </Button>
-          <Button color="gray">BUTTON</Button>
-          <Button color="gray" size="small">
-            BUTTON
-          </Button>
-        </ButtonGroup>
+          <ButtonGroup>
+            <Button color="gray" size="large">
+              BUTTON
+            </Button>
+            <Button color="gray">BUTTON</Button>
+            <Button color="gray" size="small">
+              BUTTON
+            </Button>
+          </ButtonGroup>
 
-        <ButtonGroup>
-          <Button color="pink" size="large">
-            BUTTON
-          </Button>
-          <Button color="pink">BUTTON</Button>
-          <Button color="pink" size="small">
-            BUTTON
-          </Button>
-        </ButtonGroup>
+          <ButtonGroup>
+            <Button color="pink" size="large">
+              BUTTON
+            </Button>
+            <Button color="pink">BUTTON</Button>
+            <Button color="pink" size="small">
+              BUTTON
+            </Button>
+          </ButtonGroup>
 
-        <ButtonGroup>
-          <Button size="large" outline>
-            BUTTON
-          </Button>
-          <Button color="gray" outline>
-            BUTTON
-          </Button>
-          <Button color="pink" size="small" outline>
-            BUTTON
-          </Button>
-        </ButtonGroup>
+          <ButtonGroup>
+            <Button size="large" outline>
+              BUTTON
+            </Button>
+            <Button color="gray" outline>
+              BUTTON
+            </Button>
+            <Button color="pink" size="small" outline>
+              BUTTON
+            </Button>
+          </ButtonGroup>
 
-        <ButtonGroup>
-          <Button size="large" fullwidth>
-            BUTTON
-          </Button>
-          <Button size="large" color="gray" fullwidth>
-            BUTTON
-          </Button>
-          <Button size="large" color="pink" fullwidth>
-            BUTTON
-          </Button>
-        </ButtonGroup>
-      </AppBlock>
+          <ButtonGroup>
+            <Button size="large" fullwidth>
+              BUTTON
+            </Button>
+            <Button size="large" color="gray" fullwidth>
+              BUTTON
+            </Button>
+            <Button size="large" color="pink" fullwidth onClick={onClickBtn}>
+              DELETE
+            </Button>
+          </ButtonGroup>
+        </AppBlock>
+        <Dialog
+          title="정말로 삭제하시겠습니다?"
+          confirmText="삭제"
+          cancelText="취소"
+          onCancel={onCancel}
+          onConfirm={onConfirm}
+          visible={dialog}
+        >
+          데이터를 정말로 삭제하시겠습니까?
+        </Dialog>
+      </>
     </ThemeProvider>
   );
 }
